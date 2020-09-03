b59c12341bf0ca78f5b56480b5009a75383f1c81
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-09-03 13:17:23 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -14,7 +14,7 @@ namespace Statistics.Test
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(new List<float>{1.5F, 8.9F, 3.2F, 4.5F});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.Average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.Average - 4.525) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Max - 8.9) <= epsilon);
             Assert.True(Math.Abs(statsComputer.Min - 1.5) <= epsilon);
             Console.WriteLine("ReportsAverageMinMax Complete");
@@ -24,9 +24,9 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(new List<float>{});
-            Assert.True(Double.IsNaN(computeStats.Average));
-            Assert.True(Double.IsNaN(computeStats.Max));
-            Assert.True(Double.IsNaN(computeStats.Min));
+            Assert.True(Double.IsNaN(statsComputer.Average));
+            Assert.True(Double.IsNaN(statsComputer.Max));
+            Assert.True(Double.IsNaN(statsComputer.Min));
             Console.WriteLine("ReportsNaNForEmptyInput Complete");
 
             //All fields of computedStats (average, max, min) must be
