49c0bbe139f1ed39af41ccaa755099a23bec02d1
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-09-03 12:07:55 +0530

    Add files via upload

---
+++ Statistics/Stats.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class Stats
+    {
+        public float Average { get; set; }
+        public float Max { get; set; }
+        public float Min { get; set; }
+    }
+}
